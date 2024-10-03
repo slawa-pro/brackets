ffd0118010988be323d4aff960f7a1a5755d4aa3
Vyacheslav <va.povhleb@gmail.com>
2024-10-03 14:53:57 +0300

    refactor: fix bag(Tue, Oct 03, 2024 14:57:04)

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  const openBrackets = ['(', '{']; 
-  const pairBrackets = {
-    [')']: '(',
-    ['}']: '{',
-  };
+  const openBrackets = new Set(bracketsConfig.map(pair => pair[0]));
+  const closeBrackets = new Set(bracketsConfig.map(pair => pair[1]));
+  const pairBrackets = {};
+  bracketsConfig.forEach(pair => {
+    pairBrackets[pair[1]] = pair[0];
+  });
 
-  function isBracketsOk(str) {
-    let stack = [];
+  const symmetricBrackets = new Set(
+    bracketsConfig.filter(pair => pair[0] === pair[1]).map(pair => pair[0])
+  );
 
-    for (let i = 0; i < str.length; i++) {
-      let currentSymbol = str[i];
-      
-      if (openBrackets.includes(currentSymbol)) {
-        stack.push(currentSymbol);
+  let stack = [];
+
+  for (let symbol of str) {
+    if (symmetricBrackets.has(symbol)) {
+      if (stack.length > 0 && stack[stack.length - 1] === symbol) {
+        stack.pop();
       } else {
-        if (stack.length === 0) {
-          return false;
-        }
-
-        let topElement = stack[stack.length - 1];
-
-        if (pairBrackets[currentSymbol] === topElement) {
-          stack.pop();
-        } else {
-          return false;
-        }
+        stack.push(symbol);
       }
+    } else if (openBrackets.has(symbol)) {
+      stack.push(symbol);
+    } else if (closeBrackets.has(symbol)) {
+      if (stack.length === 0 || pairBrackets[symbol] !== stack.pop()) {
+        return false;
+      }
+    } else {
+      return false;
     }
-    return stack.length === 0;
   }
 
-  return isBracketsOk(str);
+  return stack.length === 0;
 }
