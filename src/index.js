db1db7c5c34406d00396be3395725a2bc2fe5952
Vyacheslav <va.povhleb@gmail.com>
2024-10-03 12:44:43 +0300

    init: end task (Tue, Oct 03, 2024 12:43:04)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = bracketsConfig.map(pair => pair[0]);
+  const pairBrackets = {};
+  bracketsConfig.forEach(pair => {
+    pairBrackets[pair[1]] = pair[0];
+  });
+
+  function isBracketsOk(str) {
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+      let currentSymbol = str[i];
+
+      if (openBrackets.includes(currentSymbol)) {
+        stack.push(currentSymbol);
+      } else {
+        if (stack.length === 0) {
+          return false;
+        }
+
+        let topElement = stack[stack.length - 1];
+
+        if (pairBrackets[currentSymbol] === topElement) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+    return stack.length === 0;
+  }
+
+  return isBracketsOk(str);
 }
